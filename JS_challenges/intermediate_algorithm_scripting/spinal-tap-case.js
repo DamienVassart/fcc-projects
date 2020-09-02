0a79606e9e84fc350d84876c9da54f2454cbfc84
Damien Vassart <damienvassart@tutanota.com>
2020-09-02 18:35:12 +0200

    added three new JS challenges

---
+++ JS_challenges/intermediate_algorithm_scripting/spinal-tap-case.js
@@ -0,0 +1,5 @@
+// Convert a string to spinal case. Spinal case is all-lowercase-words-joined-by-dashes.
+
+function spinalCase(str) {
+  return str.replace(/([a-z])([A-Z])/g, '$1-$2').replace(/\W|_/g, '-').toLowerCase();
+}
