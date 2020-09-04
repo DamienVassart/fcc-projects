6b2512aab467d5965b255009b5731dbf1489be33
DamienVassart <damienvassart@tutanota.com>
2020-09-04 17:47:14 +0200

    refactored the code on missing-letters.js with destructuration

--- JS_challenges/intermediate_algorithm_scripting/missing-letters.js
+++ JS_challenges/intermediate_algorithm_scripting/missing-letters.js
@@ -7,10 +7,11 @@ If all letters are present in the range, return undefined.
 
 function fearNotLetter(str) {
   var strCharCodes = [...str].map(l => l.charCodeAt(0));
-  var start = str.charCodeAt(0);
-  var end = str.charCodeAt(str.length - 1);
+  var [start, end] = [str.charCodeAt(0), str.charCodeAt(str.length - 1)];
   var fullRange = [...new Array(end - start + 1).keys()].map((_, i) => i + start);
   var diff = fullRange.filter(n => strCharCodes.indexOf(n) === -1);
   
   return diff.length == 0 ? undefined : diff.map(l => String.fromCharCode(l)).join('');
 }
+
+console.log(fearNotLetter('abcde'));
