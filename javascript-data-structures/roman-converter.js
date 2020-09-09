74f1e885dd5076602c250f42a5a55df1e0f0bf26
Damien Vassart <damienvassart@tutanota.com>
2020-09-09 21:38:26 +0200

    continued the roman converter project -- some additional tests needed befor submition

--- javascript-data-structures/roman-converter.js
+++ javascript-data-structures/roman-converter.js
@@ -25,12 +25,13 @@ function convertToRoman(num) {
 		1 : 'I'
 	};
 
-	var divisors = [1000, 500, 100, 50, 10, 5, 1];
+	var divisors = [1000, 500, 50, 5];
 	//var res = divisors.reduce((res, val, i, arr) => res.concat(((num % arr[i - 1]) - num % arr[i]) / arr[i]) , [(num - num % 1000) / 1000]).filter(e => !isNaN(e));
-	var breakDown = (num + '').split('').reverse().map((e, i) => +e * 10**i).reverse();
+	var breakDown = (num + '').padStart(4, '0').split('').reverse().map((e, i) => +e * 10**i).reverse();
+	return breakDown.map((e, i) => e === 0 ? 0 : map.hasOwnProperty(e) ? map[e] : e > divisors[i] ? [map[divisors[i]], map[e % divisors[i]]].join('') : map[10**(i-1)].repeat(e % divisors[i] / 10**(i-1)));
 
-	return breakDown;
+	
 }
 
 console.log(convertToRoman(36)); // XXXVI => 10 10 10 5 1
-console.log(convertToRoman(1536)); // MDXXXVI => 1000 500 10 10 10 5 1
+console.log(convertToRoman(1546)); // MDXXXVI => 1000 500 10 10 10 5 1
