a36093a0a59d3965452fe8d45a63da9c21afe98f
Damien Vassart <damienvassart@tutanota.com>
2020-09-10 18:19:37 +0200

    finished a first working version of the roman converter

--- javascript-data-structures/roman-converter.js
+++ javascript-data-structures/roman-converter.js
@@ -9,7 +9,7 @@ https://www.mathsisfun.com/roman-numerals.html
 
 function convertToRoman(num) {
 	
-	var map = {
+	var romanMap = {
 		1000 : 'M', 
 		900 : 'CM', 
 		500 : 'D', 
@@ -25,13 +25,21 @@ function convertToRoman(num) {
 		1 : 'I'
 	};
 
-	var divisors = [1000, 500, 50, 5];
-	//var res = divisors.reduce((res, val, i, arr) => res.concat(((num % arr[i - 1]) - num % arr[i]) / arr[i]) , [(num - num % 1000) / 1000]).filter(e => !isNaN(e));
-	var breakDown = (num + '').padStart(4, '0').split('').reverse().map((e, i) => +e * 10**i).reverse();
-	return breakDown.map((e, i) => e === 0 ? 0 : map.hasOwnProperty(e) ? map[e] : e > divisors[i] ? [map[divisors[i]], map[e % divisors[i]]].join('') : map[10**(i-1)].repeat(e % divisors[i] / 10**(i-1)));
+	var divisors = [1000, 500, 100, 50, 10, 5, 1];
 
+	var split = (num + '').padStart(4, '0').split('').reverse().map((e, i) => +e * 10**i).reverse();
+
+	var breakDown = [(num - num % 1000) / 1000];
+
+	for (let i = 1; i < divisors.length; i++) {
+		breakDown.push((num % divisors[i - 1] - num % divisors[i]) / divisors[i]);
+	}
+
+	var thousands = romanMap[1000].repeat(split[0] / 1000);
+	var hundreds = romanMap.hasOwnProperty(split[1]) ? romanMap[split[1]] : [romanMap[500].repeat(breakDown[1]), romanMap[100].repeat(breakDown[2])].join('');
+	var tens = romanMap.hasOwnProperty(split[2]) ? romanMap[split[2]] : [romanMap[50].repeat(breakDown[3]), romanMap[10].repeat(breakDown[4])].join('');
+	var units = romanMap.hasOwnProperty(split[3]) ? romanMap[split[3]] : [romanMap[5].repeat(breakDown[5]), romanMap[1].repeat(breakDown[6])].join('');
+
+	return thousands + hundreds + tens + units;
 	
 }
-
-console.log(convertToRoman(36)); // XXXVI => 10 10 10 5 1
-console.log(convertToRoman(1546)); // MDXXXVI => 1000 500 10 10 10 5 1
