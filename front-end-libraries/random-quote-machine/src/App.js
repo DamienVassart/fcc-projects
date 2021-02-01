2d37fb97057d1660b5c0640743bd4eaa6289681e
DamienVassart <damienvassart@tutanota.com>
2021-02-01 10:37:18 +0100

    [Frontend Libraries -> Random Quote Machine] initial commit

---
+++ front-end-libraries/random-quote-machine/src/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+import QuoteBox from "./QuoteBox";
+
+import quotesData from "./assets/quotes"
+
+const quotesArr = quotesData.quotes
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    
+  }
+
+  render () {
+    return <div></div>
+  }
+}
+
+export default App;
---
+++ front-end-libraries/random-quote-machine/src/QuoteBox.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+class QuoteBox extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return <div></div>
+    }
+}
+
+export default QuoteBox
