f291c998d8f7ada382e551b21026fa7cbfc207fe
DamienVassart <damienvassart@tutanota.com>
2021-02-02 18:17:51 +0100

    [Frontend Libraries -> Random Quote Machine]: made the quote text font size adjusting dynamically with the text length

--- front-end-libraries/random-quote-machine/src/App.js
+++ front-end-libraries/random-quote-machine/src/App.js
@@ -5,6 +5,8 @@ import QuoteBox from "./QuoteBox";
 import quotesData from "./assets/quotes"
 
 const quotesArr = quotesData.quotes
+const quotesLengths = quotesArr.map(e => e.quote.length);
+const median = Math.floor(Math.min(...quotesLengths) + (Math.max(...quotesLengths) - Math.min(...quotesLengths)) / 2);
 
 class App extends React.Component {
   constructor(props) {
@@ -19,6 +21,7 @@ class App extends React.Component {
         text={quotesArr[n].quote}
         author={quotesArr[n].author}
         newQuote={this.newQuote}
+        median={median}
       />
     )
   }
--- front-end-libraries/random-quote-machine/src/QuoteBox.js
+++ front-end-libraries/random-quote-machine/src/QuoteBox.js
@@ -4,20 +4,31 @@ class QuoteBox extends React.Component {
     constructor(props) {
         super(props);
         this.tweetQuote = this.tweetQuote.bind(this);
+        this.setFontSize = this.setFontSize.bind(this);
     }
 
     tweetQuote(str1, str2) {
         return `https://twitter.com/intent/tweet?text="${str1}" (${str2})`;
     }
 
+    setFontSize(str, n) {
+        const coef = Math.sqrt(Math.ceil(100 / str.length));
+        const fontSize = ((coef <= 2 ? coef : 2) * (n / 100));
+        return {fontSize:  fontSize.toFixed(1) + 'rem'};
+    }
+
     render() {
+        const text = this.props.text;
+        const author = this.props.author;
+        const median = this.props.median;
+        console.log(this.setFontSize(text, median));
         return (
             <div id="quote-box">
-                <blockquote id="text">
-                    "{this.props.text}"
+                <blockquote id="text" style={this.setFontSize(text, median)}>
+                    "{text}"
                 </blockquote>
                 <p id="author">
-                    -- {this.props.author}
+                    -- {author}
                 </p>
                 <div id="new-quote-wrapper" className="wrapper">
                     <button id="new-quote" onClick={this.props.newQuote}>
@@ -25,7 +36,7 @@ class QuoteBox extends React.Component {
                     </button>
                 </div>
                 <div id="tweet-quote-wrapper" className="wrapper">
-                    <a href={this.tweetQuote(this.props.text, this.props.author)} id="tweet-quote" target="_blank" rel="noreferrer">
+                    <a href={this.tweetQuote(text, author)} id="tweet-quote" target="_blank" rel="noreferrer">
                         <button>
                             Tweet Quote
                         </button>
